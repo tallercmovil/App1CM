58cc0574d72f1af836ed1e7b230f7b8d531993e4
Amaury Perea Matsumura <amaury.matsumura@gmail.com>
2024-08-30 11:25:41 -0600

    Ejercicio finalizado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.amaurypm.app1cm"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -33,6 +33,8 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+
 }
 
 dependencies {
--- app/src/main/java/com/amaurypm/app1cm/test2.kt
+++ app/src/main/java/com/amaurypm/app1cm/test2.kt
@@ -1,15 +1,21 @@
 package com.amaurypm.app1cm
 
-fun main(){
+import java.text.DecimalFormat
+import kotlin.math.pow
+import kotlin.random.Random
+
+
+fun main() {
     //println("Hola ejemplo 2")
 
-    fun sumaNumeros(a: Int, b: Int){
-        println("La suma de $a + $b es ${a+b}")
+    fun sumaNumeros(a: Int, b: Int) {
+        println("La suma de $a + $b es ${a + b}")
     }
+
 
     //sumaNumeros(67,31)
 
-    /*class Prueba: Operaciones{
+    class Prueba : Operaciones {
         override fun suma(num1: Int, num2: Int) {
 
         }
@@ -18,38 +24,87 @@ fun main(){
 
         }
 
-    }*/
+    }
 
-    fun operaNumeros(a: Int, b: Int, operaciones: Operaciones){
+    fun operaNumeros(a: Int, b: Int, operaciones: Operaciones) {
         operaciones.suma(a, b)
         operaciones.resta(a, b)
     }
 
-    operaNumeros(78, 25, object: Operaciones{
+    fun operaNumeros(a: Int, b: Int, operacion: (Int, Int) -> Unit) {
+        //val c = a*b
+        operacion(a, b)
+    }
+
+    fun operaNumeros(
+        a: Int,
+        b: Int,
+        lambda1: (String) -> Boolean,
+        lambda2: (Double, Boolean) -> Unit
+    ) {
+
+    }
+
+    operaNumeros(25, 50,
+        { texto ->
+            return@operaNumeros true
+        }, { miDouble, miBoolean ->
+
+        })
+
+    val miLambdaSuma: (Int, Int) -> Unit =
+        { num1, num2 -> println("La suma con lambda de $num1 + $num2 es ${num1 + num2}") }
+
+
+    operaNumeros(28, 46) { num1, num2 ->
+        println("La suma con lambda de $num1 + $num2 es ${num1 + num2}")
+    }
+
+    operaNumeros(28, 46) { num1, num2 ->
+        println("La multiplicación con lambda de $num1 * $num2 es ${num1 * num2}")
+    }
+
+    fun operaNumeroAleatorio(operacion: (Int) -> Unit) {
+        val num = Random.nextInt(0, 100)
+        operacion(num)
+    }
+
+    operaNumeroAleatorio { numAleatorio ->
+        val resultado = numAleatorio * numAleatorio
+        println("El cuadrado de $numAleatorio es $resultado")
+    }
+
+    operaNumeroAleatorio { numAleatorio ->
+        val resultado = numAleatorio.toDouble().pow(3.0)
+        //println("El cubo de $numAleatorio es ${String.format( "%.2f" ,resultado)}")
+        val decimalFormat = DecimalFormat("###,###,###.00")
+        println("El cubo de $numAleatorio es \$${decimalFormat.format(resultado)}")
+    }
+
+    operaNumeros(78, 25, object : Operaciones {
         override fun suma(num1: Int, num2: Int) {
-            println("La suma de $num1 + $num2 es ${num1+num2}")
+            println("La suma de $num1 + $num2 es ${num1 + num2}")
         }
 
         override fun resta(num1: Int, num2: Int) {
-            println("La resta de $num1 - $num2 es ${num1-num2}")
+            println("La resta de $num1 - $num2 es ${num1 - num2}")
         }
-
     })
 
-    operaNumeros(78, 25, object: Operaciones{
+    operaNumeros(78, 25, object : Operaciones {
         override fun suma(num1: Int, num2: Int) {
-            println("El doble de la suma de $num1 + $num2 es ${2*(num1+num2)}")
+            println("El doble de la suma de $num1 + $num2 es ${2 * (num1 + num2)}")
         }
 
         override fun resta(num1: Int, num2: Int) {
-            println("El triple de la resta de $num1 - $num2 es ${3*(num1-num2)}")
+            println("El triple de la resta de $num1 - $num2 es ${3 * (num1 - num2)}")
         }
 
     })
 
 }
 
-interface Operaciones{
+interface Operaciones {
     fun suma(num1: Int, num2: Int)
     fun resta(num1: Int, num2: Int)
 }
